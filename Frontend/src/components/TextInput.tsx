97d757070cd6a045f5060210662254d4b74ade61
Xiao-honsu <vohoangduc3012@gmail.com>
2025-02-19 08:36:08 +0700

    Add global component - TextInput

--- Frontend/src/components/TextInput.tsx
+++ Frontend/src/components/TextInput.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 
-const TextInput = ({ type = "text", label, placeholder, onChange, name }) => {
+type TextInputProps = {
+  type?: string;
+  placeholder?: string;
+  onChange: (value: string) => void;
+  label?: string;
+  name: string;
+};
+
+const TextInput: React.FC<TextInputProps> = ({
+  type = "text",
+  label,
+  placeholder,
+  onChange,
+  name,
+}) => {
   return (
     <div className="flex flex-col space-y-2">
-      {label && <label className="text-gray-700 font-medium" htmlFor={name}>{label}</label>}
+      {label && (
+        <label className="text-gray-700 font-medium" htmlFor={name}>
+          {label}
+        </label>
+      )}
       <input
         type={type}
         name={name}
