bfddbeab50d8ebacbac5590d5fc696abaffe081a
ketamean <truongthanhtoan2003@gmail.com>
2025-03-05 12:17:16 +0700

    grant select column configuration sql script

--- Backend/config/database/setupQueries/enums.sql
+++ Backend/config/database/setupQueries/enums.sql
@@ -1,35 +1,29 @@
-CREATE OR REPLACE PROCEDURE __create_enums()
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    -- public.MaterialType
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'materialtype' AND typnamespace = 'public'::regnamespace) THEN
-        CREATE TYPE "public".MaterialType AS ENUM ('Quiz', 'Document', 'Video');
-    END IF;
+-- public.MaterialType
+IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'materialtype' AND typnamespace = 'public'::regnamespace) THEN
+    CREATE TYPE "public".MaterialType AS ENUM ('Quiz', 'Document', 'Video');
+END IF;
 
-    -- public.CourseStatus
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'coursestatus' AND typnamespace = 'public'::regnamespace) THEN
-        CREATE TYPE "public".CourseStatus AS ENUM ('InReview', 'Published', 'Hidden');
-    END IF;
+-- public.CourseStatus
+IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'coursestatus' AND typnamespace = 'public'::regnamespace) THEN
+    CREATE TYPE "public".CourseStatus AS ENUM ('InReview', 'Published', 'Hidden');
+END IF;
 
-    -- private.CouponDiscountType
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'coupondiscounttype' AND typnamespace = 'private'::regnamespace) THEN
-        CREATE TYPE "private".CouponDiscountType AS ENUM ('ValueBased', 'PercentageBased');
-    END IF;
+-- private.CouponDiscountType
+IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'coupondiscounttype' AND typnamespace = 'private'::regnamespace) THEN
+    CREATE TYPE "private".CouponDiscountType AS ENUM ('ValueBased', 'PercentageBased');
+END IF;
 
-    -- private.UserType
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'usertype' AND typnamespace = 'private'::regnamespace) THEN
-        CREATE TYPE "private".UserType AS ENUM ('Learner', 'Instructor', 'Admin');
-    END IF;
+-- private.UserType
+IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'usertype' AND typnamespace = 'private'::regnamespace) THEN
+    CREATE TYPE "private".UserType AS ENUM ('Learner', 'Instructor', 'Admin');
+END IF;
 
-    -- private.QuizQuestionType
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'quizquestiontype' AND typnamespace = 'private'::regnamespace) THEN
-        CREATE TYPE "private".QuizQuestionType AS ENUM ('MultipleChoices', 'ConstructedResponse');
-    END IF;
+-- private.QuizQuestionType
+IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'quizquestiontype' AND typnamespace = 'private'::regnamespace) THEN
+    CREATE TYPE "private".QuizQuestionType AS ENUM ('MultipleChoices', 'ConstructedResponse');
+END IF;
 
-    -- private.UserAccountStatus
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'useraccountstatus' AND typnamespace = 'private'::regnamespace) THEN
-        CREATE TYPE "private".UserAccountStatus AS ENUM ('Unconfirmed', 'Confirmed', 'Banned');
-    END IF;
-END;
-$$;
+-- private.UserAccountStatus
+IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'useraccountstatus' AND typnamespace = 'private'::regnamespace) THEN
+    CREATE TYPE "private".UserAccountStatus AS ENUM ('Unconfirmed', 'Confirmed', 'Banned');
+END IF;
