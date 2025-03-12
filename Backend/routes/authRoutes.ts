d8864ca80537d9594397857b9ecbf515a15596bc
nnnnhu22@clc.fitus.edu.vn <144573999+ngocnhu100@users.noreply.github.com>
2025-03-12 12:29:43 +0700

    Added payment with stripe

--- Backend/routes/authRoutes.ts
+++ Backend/routes/authRoutes.ts
@@ -1,7 +1,7 @@
-import express from "express";
+// import express from "express";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.use();
+// router.use();
 
-export default router;
+// export default router;
---
+++ Backend/services/stripe.ts
@@ -0,0 +1,5 @@
+import "dotenv/config";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default stripe;
