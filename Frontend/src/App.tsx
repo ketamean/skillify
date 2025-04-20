0bd2c0cd2876e604d7b99d73cd8a52d21a2f330c
ketamean <truongthanhtoan2003@gmail.com>
2025-04-21 01:54:03 +0700

    update dependencies

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Homepage from "./pages/Homepage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import CoursePage from "./pages/CoursePage";
-import UploadCourseContentPage from "./pages/UploadCourseContentPage";
+import UploadCourseContentPage from "./pages/CourseEdit";
 import CourseContentPage from "./pages/CourseContent"
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
