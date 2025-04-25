597303cb348c4b7435537aacf47fc072f06adf6d
Xiao-honsu <vohoangduc3012@gmail.com>
2025-04-25 22:02:20 +0700

    view quiz stat

--- Backend/routes/quizzRoutes.ts
+++ Backend/routes/quizzRoutes.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
-import { getQuizStats, addQuizzAttemp } from "../controllers/quizzController";
+import { getQuizStats, addQuizzAttemp,getUserQuiz } from "../controllers/quizzController";
 import supabase from "../config/database/supabase";
 import { error } from "console";
 
 const router = express.Router();
 
 router.get("/:quiz_id", getQuizStats);
+router.get("/myquiz/:user_id", getUserQuiz);
 router.post("/:quiz_id", addQuizzAttemp);
 
 
